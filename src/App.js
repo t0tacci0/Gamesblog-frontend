239e4696713048843f2e39a4c2aaf6518995b15a
Antonio Cesarino <ant.ces86@outlook.com>
2024-08-26 23:18:39 +0000

    Add react-bootstrap library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Button from "react-bootstrap/Button";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello!!!</h1>
+      <Button variant="success">Success</Button>
     </div>
   );
 }
