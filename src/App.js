05a1715c27dc51bf28c5943ed05175daa74edfd8
Antonio Cesarino <ant.ces86@outlook.com>
2024-08-27 19:14:26 +0000

    Add navbar,favicon and logo project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import Button from "react-bootstrap/Button";
 import './App.css';
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!!!</h1>
-      <Button variant="success">Success</Button>
+      <NavBar />
     </div>
   );
 }
