709f9084caefd2dabb02249698de4ac02daea811
Antonio Cesarino <ant.ces86@outlook.com>
2024-09-20 00:05:53 +0000

    Add redirecting user page

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { Form, Button, Image, Col, Row, Container, Alert } from "react-bootstrap";
 import axios from "axios";
+import { useRedirect } from "../../hooks/useRedirect";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
 const SignUpForm = () => {
+  useRedirect("loggedIn");
   const [signUpData, setSignUpData] = useState({
     username: "",
     password1: "",
