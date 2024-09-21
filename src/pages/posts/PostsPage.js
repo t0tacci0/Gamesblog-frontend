ead896189e5f59f8c164c4e88d8846340f0c47f1
Antonio Cesarino <ant.ces86@outlook.com>
2024-09-21 20:38:24 +0000

    Add Procfile and install heroku package

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -13,11 +13,14 @@ import NoResults from "../../assets/no-results.png";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
 import PopularProfiles from "../profiles/PopularProfiles";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
   const [query, setQuery] = useState("");
+  const currentUser = useCurrentUser();
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -35,7 +38,7 @@ function PostsPage({ message, filter = "" }) {
     return () => {
       clearTimeout(timer);
     };
-  }, [filter, query, pathname]);
+  }, [filter, query, pathname, currentUser]);
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
